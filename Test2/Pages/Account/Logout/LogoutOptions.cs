e8f145c2643d9d3c5c83df3a868e252c133e32ae
Serhii Maslov <2021elit0049@ms.sumdu.edu.ua>
2025-03-18 19:55:52 +0200

    Add project files.

---
+++ Test2/Pages/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Duende Software. All rights reserved.
+// See LICENSE in the project root for license information.
+
+
+namespace Test2.Pages.Logout;
+
+public static class LogoutOptions
+{
+    public static readonly bool ShowLogoutPrompt = true;
+    public static readonly bool AutomaticRedirectAfterSignOut = false;
+}
